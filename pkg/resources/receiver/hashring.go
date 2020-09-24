95ba342dfe5dac87c28492d4facaea2c658314c5
tarokkk <gubasanyi@gmail.com>
2020-09-24 13:30:39 +0200

    add receiver component initial commit

---
+++ pkg/resources/receiver/hashring.go
@@ -0,0 +1,76 @@
+package receiver
+
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/banzaicloud/operator-tools/pkg/reconciler"
+	"github.com/banzaicloud/thanos-operator/pkg/sdk/api/v1alpha1"
+	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+)
+
+type HashRingGroup struct {
+	HashRing  string   `json:"hashring,omitempty"`
+	Endpoints []string `json:"endpoints,omitempty"`
+	Tenants   []string `json:"tenants,omitempty"`
+}
+
+//func (r *Receiver) GetEndpointsForTenant(tenantName string) []string{
+//	var endpoints []string
+//	for _, receiverGroup := range r.ReceiverGroups {
+//		if contains(receiverGroup.Tenants, tenantName) {
+//			endpoints = append(endpoints, receiverGroup.Tenants...)
+//		}
+//	}
+//	return endpoints
+//}
+//
+//// Check if a string contains an element
+//func contains(list []string, element string) bool {
+//	for _, e := range list {
+//		if e == element {
+//			return true
+//		}
+//	}
+//	return false
+//}
+
+func (r *Receiver) generateEndpointsForGroup(group v1alpha1.ReceiverGroup) []string {
+	var endpoints []string
+	for i := 0; i < group.Replicas; i++ {
+		endpoint := fmt.Sprintf("%s-%d.%s.svc.%s", group.GroupName, i, group.Namespace, r.Thanos.GetClusterDomain())
+		endpoints = append(endpoints, endpoint)
+	}
+	return endpoints
+}
+
+func (r *Receiver) GenerateHashring() (string, error) {
+	hashringConfig := make([]HashRingGroup, len(r.ReceiverObject.Spec.ReceiverGroups))
+	for i, receiverGroup := range r.ReceiverObject.Spec.ReceiverGroups {
+		hashringConfig[i].HashRing = receiverGroup.GroupName
+		hashringConfig[i].Tenants = receiverGroup.Tenants
+		hashringConfig[i].Endpoints = r.generateEndpointsForGroup(receiverGroup)
+	}
+	result, err := json.Marshal(hashringConfig)
+	if err != nil {
+		return "", err
+	}
+	return string(result), nil
+}
+
+func (r *Receiver) hashring() (runtime.Object, reconciler.DesiredState, error) {
+	configuration, err := r.GenerateHashring()
+	if err != nil {
+		return nil, nil, err
+	}
+	configmap := &v1.ConfigMap{
+		TypeMeta:   metav1.TypeMeta{},
+		ObjectMeta: metav1.ObjectMeta{},
+		Data: map[string]string{
+			"hashring.json": configuration,
+		},
+	}
+	return configmap, reconciler.StatePresent, nil
+}
---
+++ pkg/resources/receiver/receiver.go
@@ -0,0 +1,143 @@
+// Copyright 2020 Banzai Cloud
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package receiver
+
+import (
+	"fmt"
+
+	"github.com/banzaicloud/operator-tools/pkg/utils"
+	"github.com/banzaicloud/thanos-operator/pkg/resources"
+	"github.com/banzaicloud/thanos-operator/pkg/sdk/api/v1alpha1"
+	"github.com/imdario/mergo"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
+)
+
+type Receiver struct {
+	ReceiverObject *v1alpha1.Receiver
+	*resources.ThanosComponentReconciler
+}
+
+type receiverInstance struct {
+	*Receiver
+	receiverGroup *v1alpha1.ReceiverGroup
+}
+
+func (r *receiverInstance) getName(suffix ...string) string {
+	name := r.QualifiedName(v1alpha1.ReceiverName)
+	if len(suffix) > 0 && suffix[0] != "" {
+		name = name + "-" + suffix[0]
+	}
+	return name
+}
+
+func (r *receiverInstance) getVolumeMeta(name string) metav1.ObjectMeta {
+	meta := r.GetNameMeta(name, r.receiverGroup.Namespace)
+	meta.OwnerReferences = []metav1.OwnerReference{
+		{
+			APIVersion: r.ReceiverObject.APIVersion,
+			Kind:       r.ReceiverObject.Kind,
+			Name:       r.ReceiverObject.Name,
+			UID:        r.ReceiverObject.UID,
+			Controller: utils.BoolPointer(true),
+		},
+	}
+	meta.Labels = r.getLabels()
+	return meta
+}
+
+func (r *receiverInstance) getMeta(suffix ...string) metav1.ObjectMeta {
+	nameSuffix := ""
+	if len(suffix) > 0 {
+		nameSuffix = suffix[0]
+	}
+	meta := r.GetNameMeta(r.getName(nameSuffix), "")
+	meta.OwnerReferences = []metav1.OwnerReference{
+		{
+			APIVersion: r.ReceiverObject.APIVersion,
+			Kind:       r.ReceiverObject.Kind,
+			Name:       r.ReceiverObject.Name,
+			UID:        r.ReceiverObject.UID,
+			Controller: utils.BoolPointer(true),
+		},
+	}
+	meta.Labels = r.getLabels()
+	return meta
+}
+
+func (r *receiverInstance) getSvc() string {
+	return fmt.Sprintf("_grpc._tcp.%s.%s.svc.cluster.local", r.getName(), r.receiverGroup.Namespace)
+}
+
+func New(reconciler *resources.ThanosComponentReconciler) *Receiver {
+	return &Receiver{
+		ThanosComponentReconciler: reconciler,
+	}
+}
+
+func (r *Receiver) resourceFactory() []resources.Resource {
+	var resourceList []resources.Resource
+
+	for _, group := range r.ReceiverObject.Spec.ReceiverGroups {
+		resourceList = append(resourceList, (&receiverInstance{r, group.DeepCopy()}).statefulset)
+		resourceList = append(resourceList, (&receiverInstance{r, group.DeepCopy()}).hashring)
+		//resourceList = append(resourceList, (&receiverInstance{r, group.DeepCopy()}).service)
+		//resourceList = append(resourceList, (&receiverInstance{r, group.DeepCopy()}).serviceMonitor)
+		//resourceList = append(resourceList, (&receiverInstance{r, group.DeepCopy()}).ingressHTTP)
+		//resourceList = append(resourceList, (&receiverInstance{r, group.DeepCopy()}).ingressGRPC)
+	}
+
+	return resourceList
+}
+
+//func (r *Receiver) GetServiceURLS() []string {
+//	var urls []string
+//	for _, endpoint := range r.StoreEndpoints {
+//		urls = append(urls, (&receiverInstance{r, endpoint.DeepCopy()}).getSvc())
+//	}
+//	return urls
+//}
+
+func (r *Receiver) Reconcile() (*reconcile.Result, error) {
+	if r.Thanos.Spec.Rule != nil {
+		err := mergo.Merge(r.Thanos.Spec.Rule, v1alpha1.DefaultRule)
+		if err != nil {
+			return nil, err
+		}
+	}
+	return r.ReconcileResources(r.resourceFactory())
+}
+
+func (r *receiverInstance) getLabels() resources.Labels {
+	labels := resources.Labels{
+		resources.NameLabel: v1alpha1.ReceiverName,
+	}.Merge(
+		r.GetCommonLabels(),
+	)
+	if r.Thanos.Spec.Rule != nil {
+		labels.Merge(r.Thanos.Spec.Rule.Labels)
+	}
+	return labels
+}
+
+func (r *receiverInstance) setArgs(args []string) []string {
+	args = append(args, resources.GetArgs(r.receiverGroup)...)
+
+	//Label
+
+	// Local-endpoint
+
+	return args
+}
